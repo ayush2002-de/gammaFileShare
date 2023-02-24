06cce0ea59fbfd0a6ef8ccb81d2bab9b906cef83
ayush2002-de <ayush.kumar0209@gmail.com>
2023-02-24 22:42:52 +0530

    cors add

--- server.js
+++ server.js
@@ -2,12 +2,19 @@ const express=require('express');
 const app= express();
 const connectDB=require('./config/db.js');
 const path=require('path');
+const cors=require('cors');
 
 //template engine
 app.set('views',path.join(__dirname,'/views'));
 app.set('view engine','ejs');
 app.use(express.static('public'));
 app.use(express.json());
+
+//cors
+const corsOption={
+    origin:process.env.ALLOW_CLIENT.split(",")
+}
+app.use(cors(corsOption));
 
 const PORT=process.env.PORT || 3000;
 connectDB();
